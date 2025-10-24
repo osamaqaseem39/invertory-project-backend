478df1279b86ec4f51dfba9ea71406a0dc4573a7
obaid-ur-rahman-zia <obaid107333@gmail.com>
2025-10-24 14:34:16 +0500

    added the netlify.toml and edited vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
+import netlify from "@netlify/vite-plugin";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), netlify()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
